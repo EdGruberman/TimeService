06019d45c0a8a8976ff4ebed2c3d97495a3200e4
EdGruberman <ed@rjump.com>
2012-08-07 14:34:41 -0700

    Initial API reference

---
+++ src/edgruberman/bukkit/timeservice/TimeService.java
@@ -0,0 +1,117 @@
+package edgruberman.bukkit.timeservice;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.charset.Charset;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import java.util.logging.Handler;
+import java.util.logging.Level;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import edgruberman.bukkit.timeservice.commands.Reload;
+
+public final class TimeService extends JavaPlugin {
+
+    private static final Version MINIMUM_CONFIGURATION = new Version("0.0.0a0");
+
+    public static Messenger messenger;
+
+    @Override
+    public void onEnable() {
+        this.reloadConfig();
+        TimeService.messenger = new Messenger(this);
+        //this.publisher = new Publisher(this, this.getDefaultRules(), this.getConfig().getStringList("exclude"));
+        this.getCommand("timeservice:reload").setExecutor(new Reload(this));
+    }
+
+    @Override
+    public void onDisable() {
+        TimeService.messenger = null;
+    }
+
+    @Override
+    public void reloadConfig() {
+        this.saveDefaultConfig();
+        super.reloadConfig();
+        this.setLogLevel(this.getConfig().getString("logLevel"));
+
+        final Version version = new Version(this.getConfig().isSet("version") ? this.getConfig().getString("version") : null);
+        if (version.compareTo(TimeService.MINIMUM_CONFIGURATION) >= 0) return;
+
+        this.archiveConfig("config.yml", version);
+        this.saveDefaultConfig();
+        this.reloadConfig();
+    }
+
+    @Override
+    public void saveDefaultConfig() {
+        this.extractConfig("config.yml", false);
+    }
+
+    private void archiveConfig(final String resource, final Version version) {
+        final String backupName = "%1$s - Archive version %2$s - %3$tY%3$tm%3$tdT%3$tH%3$tM%3$tS.yml";
+        final File backup = new File(this.getDataFolder(), String.format(backupName, resource.replaceAll("(?i)\\.yml$", ""), version, new Date()));
+        final File existing = new File(this.getDataFolder(), resource);
+
+        if (!existing.renameTo(backup))
+            throw new IllegalStateException("Unable to archive configuration file \"" + existing.getPath() + "\" with version \"" + version + "\" to \"" + backup.getPath() + "\"");
+
+        this.getLogger().warning("Archived configuration file \"" + existing.getPath() + "\" with version \"" + version + "\" to \"" + backup.getPath() + "\"");
+    }
+
+    private void extractConfig(final String resource, final boolean replace) {
+        final Charset source = Charset.forName("UTF-8");
+        final Charset target = Charset.defaultCharset();
+        if (target.equals(source)) {
+            super.saveResource(resource, replace);
+            return;
+        }
+
+        final File config = new File(this.getDataFolder(), resource);
+        if (config.exists()) return;
+
+        final char[] cbuf = new char[1024]; int read;
+        try {
+            final Reader in = new BufferedReader(new InputStreamReader(this.getResource(resource), source));
+            final Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(config), target));
+            while((read = in.read(cbuf)) > 0) out.write(cbuf, 0, read);
+            out.close(); in.close();
+
+        } catch (final Exception e) {
+            throw new IllegalArgumentException("Could not extract configuration file \"" + resource + "\" to " + config.getPath() + "\";" + e.getClass().getName() + ": " + e.getMessage());
+        }
+    }
+
+    private void setLogLevel(final String name) {
+        Level level;
+        try { level = Level.parse(name); } catch (final Exception e) {
+            level = Level.INFO;
+            this.getLogger().warning("Log level defaulted to " + level.getName() + "; Unrecognized java.util.logging.Level: " + name);
+        }
+
+        // Only set the parent handler lower if necessary, otherwise leave it alone for other configurations that have set it
+        for (final Handler h : this.getLogger().getParent().getHandlers())
+            if (h.getLevel().intValue() > level.intValue()) h.setLevel(level);
+
+        this.getLogger().setLevel(level);
+        this.getLogger().config("Log level set to: " + this.getLogger().getLevel());
+    }
+
+    public TimeZone getTimeZone(final String player) {
+        return null;
+    }
+
+    public Calendar getNow(final String player) {
+        return null;
+    }
+
+}
---
+++ src/edgruberman/bukkit/timeservice/commands/Reload.java
@@ -0,0 +1,26 @@
+package edgruberman.bukkit.timeservice.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+
+import edgruberman.bukkit.timeservice.TimeService;
+
+public final class Reload implements CommandExecutor {
+
+    private final Plugin plugin;
+
+    public Reload(final Plugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+        this.plugin.onDisable();
+        this.plugin.onEnable();
+        TimeService.messenger.tell(sender, "messages.reload");
+        return true;
+    }
+
+}
